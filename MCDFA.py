60426b88f582787af50ad6174756cab0ec3d86b6
Miner205 <arthurdonnat05@gmail.com>
2025-03-22 21:45:49 +0100

    add display of transitions in minimization

--- MCDFA.py
+++ MCDFA.py
@@ -12,6 +12,7 @@ def minimize(fa: FA) -> FA:
     :param complete deterministic fa:
     :return minimized complete deterministic fa:
     """
+    print("For Minimization, we transform the FA in CDFA without non-accessible states if it was not already the case :")
     import copy
     # we copy fa to not modify the initial fa
     fa = FA(fa.entries_number, fa.terminal_number, fa.entries.copy(), fa.terminals.copy(), copy.deepcopy(fa.nodes))
@@ -37,13 +38,15 @@ def minimize(fa: FA) -> FA:
             tmp.append(e)
     new_groups[1] = tmp  # non-terminals states
     i = 0
+    print("\nStart of Minimization :\n")
     print(f"instance n°0")
-    print("the groups are : ")
+    print("the groups (of the partition 0) are : ")
     for k in new_groups.keys():
         print('- group '+str(k) + ": ", end="")
         for elt in new_groups[k]:
             print(elt, end=", ")
         print()
+    print()
 
     while groups != new_groups:  # stop when two successive iterations give the same results
         groups = new_groups
@@ -78,25 +81,50 @@ recognized from one state and not recognized from the other. """
                         new_groups[len(groups)+len(new_groups)] = g
             else:
                 new_groups[group_k] = groups[group_k]  # we keep groups with only 1 state
-        print("display transition ...")
 
+        print("New Transitions :")
+
+        initials = []
+        terminals = []
+        n_nodes = {}
+        old_nodes = copy.deepcopy(fa.nodes)
+        for sta in new_groups.values():  # sta = states (plusieurs state)
+            curr_transitions = old_nodes[sta[0]]
+            for i_tr in range(1, len(curr_transitions)):  # start to 1 to ignore the column epsilon
+                for sta2 in new_groups.values():
+                    if set_to_str(curr_transitions[i_tr]) in set_to_str(sta2):
+                        curr_transitions[i_tr] = [set_to_str(sta2)]
+            n_nodes[set_to_str(sta)] = curr_transitions
+
+            for s in sta:  # s = state
+                if s in fa.entries:
+                    if set_to_str(sta) not in initials:
+                        initials.append(set_to_str(sta))
+                if s in fa.terminals:
+                    if set_to_str(sta) not in terminals:
+                        terminals.append(set_to_str(sta))
+        transition_f = FA(len(initials), len(terminals), initials, terminals, n_nodes)
+        display(transition_f)
+
+        i += 1
         print(f"instance n°{i}")
-        print("the groups are : ")
+        print(f"the groups (of the partition {i}) are : ")
         for k in new_groups.keys():
             print('- group ' + str(k) + ": ", end="")
             for elt in new_groups[k]:
                 print(elt, end=", ")
             print()
+        print()
 
-        i += 1
     # final instance :
     print("final instance")
-    print("the groups are : ")
+    print("the groups (of the final partition) are : ")
     for k in new_groups.keys():
         print('- group ' + str(k) + ": ", end="")
         for elt in new_groups[k]:
             print(elt, end=", ")
         print()
+    print()
 
     initials = []
     terminals = []
